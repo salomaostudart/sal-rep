a11b8f55aec0bb64c1464b3df987c4d275183b3c
salomaostudart <salomaomstudart@gmail.com>
2024-11-18 17:47:57 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é o cara! Obrigado por estar me ajudando a ser uma pessoa melhor e um bom profissional.</h1>
+    return <h1>Você é o cara! Obrigado por estar me ajudando a ser uma pessoa melhor e um bom profissional. 💪</h1>
 }
 
 export default Home;
