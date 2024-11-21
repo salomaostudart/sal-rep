6b3e365767e4fb6c3e842bb31eaef99c92ce2823
salomaostudart <salomaomstudart@gmail.com>
2024-11-21 16:54:56 +0000

    adiciona scripts `link:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Você é o cara! Obrigado por estar me ajudando a ser uma pessoa melhor e um bom profissional. 💪</h1>
+  return (
+    <h1>
+      Você é o cara! Obrigado por estar me ajudando a ser uma pessoa melhor e um
+      bom profissional. 💪
+    </h1>
+  );
 }
 
 export default Home;
